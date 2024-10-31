cc63f873de2026833957e0269c15f2dc2f8d1d11
Nguyễn Duy Thành <166360116+Tearsofroses@users.noreply.github.com>
2024-10-31 22:05:46 +0700

    Add files via upload

--- Softmax.cpp
+++ Softmax.cpp
@@ -33,16 +33,31 @@ Softmax::~Softmax() {}
 
 xt::xarray<double> Softmax::forward(xt::xarray<double> X) {
   // Todo CODE YOUR
-  m_aCached_Y = xt::exp(X) / xt::sum(xt::exp(X), m_nAxis);
+  if (X.dimension() < 2) 
+    this->m_aCached_Y = softmax(X);
+  else {
+    for (int i = 0; i < X.shape()[0]; i++) {
+      xt::view(m_aCached_Y, i, xt::all()) = softmax(xt::view(X, i, xt::all()));
+    }
+  }
   return m_aCached_Y;
 }
 
 xt::xarray<double> Softmax::backward(xt::xarray<double> DY) {
   // Todo CODE YOUR
-  xt::xarray<double> diag_y = xt::diag(m_aCached_Y);
-  xt::xarray<double> y_yt = xt::linalg::outer(m_aCached_Y, m_aCached_Y);
-  xt::xarray<double> jacobian = diag_y - y_yt;
-  return xt::linalg::dot(jacobian, DY);
+  xt::xarray<double> Z(m_aCached_Y.shape());
+  if (Z.dimension() < 2) {
+    xt::xarray<double> jacobian = xt::diag(m_aCached_Y) - xt::linalg::outer(m_aCached_Y, m_aCached_Y);
+    Z = xt::linalg::dot(DY, jacobian);
+  }
+  else {
+    for (int i = 0; i < m_aCached_Y.shape()[0]; i++) {
+      xt::xarray<double> row = xt::view(m_aCached_Y, i, xt::all());
+      xt::xarray<double> jacobian = xt::diag(row) - xt::linalg::outer(row, row);
+      xt::view(Z, i, xt::all()) = xt::linalg::dot(jacobian, xt::view(DY, i, xt::all()));
+    }
+  }
+  return Z;
 }
 
 string Softmax::get_desc() {
